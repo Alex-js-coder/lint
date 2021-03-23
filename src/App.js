b2bdf5594d8442d43dea6dc4c7535fbedadcea1b
Alexandr <alexandr@outcubator.com>
2021-03-23 13:48:02 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code >srcasdsdsad     /  App.js</code> ansakld  kalsdd save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p> adasdasd
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
